5cfcd64e28f112a796915a81c74e8df012a930e7
Shafi-shafs <128954925+Shafi-shafs@users.noreply.github.com>
2024-01-24 12:02:25 +0530

    Add files via upload

---
+++ shafi/MERN/JS/start/control.js
@@ -0,0 +1,17 @@
+if (marks > 70) {
+
+    console.log("Frist Class");
+
+} else if (marks < 70) {
+
+    console.log("Secound class");
+
+} else if (marks > 60){
+
+    console.log("Third Class");
+}else{
+
+    console.log("fail")
+}
+
+var marks= 55;
---
+++ shafi/MERN/JS/start/hoisting.js
@@ -0,0 +1,13 @@
+// Hoisting
+
+s = 20;
+console.log(s)
+var s;
+
+// temporal dead zone
+
+console.log(number);
+
+number = 10
+
+let number;
